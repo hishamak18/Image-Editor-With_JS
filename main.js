6cfaf7a438cc7a5ca2dec474ee64860955cb8cff
hisham ak <hishamak2004@gmail.com>
2022-12-28 14:57:54 +0530

    Getting Img From User And Displaying it

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const fileInput= document.querySelector('.input-file'),
- chooseImg= document.querySelector('.choose-img');
+ chooseImg= document.querySelector('.choose-img')
+ previewImg=document.querySelector('.preview-img img');
 
+
+
+const getImg = ()=>{    //**Getting Images From User */
+    let file =fileInput.files[0];
+    if(!file) return; 
+    console.log(file)
+    previewImg.src= URL.createObjectURL(file) //** Passing File and add  to PreviewImg src */
+}
+
+ fileInput.addEventListener('change',getImg)
  chooseImg.addEventListener('click',()=>fileInput.click())
